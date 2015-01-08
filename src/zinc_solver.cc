838108a4695041c4a724ed1c5a3a143c1487b55f
spinute <spinutids@gmail.com>
2015-01-09 00:40:50 +0900

    experiment: 1s, gurobi, 4core

--- src/zinc_solver.cc
+++
@@ -1,2 +0,0 @@
-~/fastdownward/src/translate/translate.py ~/fastdownward/benchmarks/blocks/domain.pddl ~/fastdownward/benchmarks/blocks/probBLOCKS-10-0.pddl
-mv output.sas ~/Dropbox/program/parse2zinc/tmp
